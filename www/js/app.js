a759aa818535ea44678c9de08d10fa1c1898d5df
Ajaylouis <s.ajaylouis@gmail.com>
2015-07-05 10:14:47 +0530

    - Slide menu

--- www/js/app.js
+++ www/js/app.js
@@ -3,81 +3,95 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('vpt', ['ionic', 'vpt.controllers', 'vpt.services'])
+angular.module('starter', ['ionic', 'starter.controllers'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+    if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
     if (window.StatusBar) {
       // org.apache.cordova.statusbar required
-      StatusBar.styleLightContent();
+      StatusBar.styleDefault();
     }
   });
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
-
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
   $stateProvider
 
-  // setup an abstract state for the tabs directive
-    .state('tab', {
-    url: "/tab",
+  .state('app', {
+    url: "/app",
     abstract: true,
-    templateUrl: "templates/tabs.html"
+    templateUrl: "templates/menu.html",
+    controller: 'AppCtrl'
   })
 
-  // Each tab has its own nav history stack:
-
-  .state('tab.walkins', {
-    url: '/walkins',
-    views: {
-      'tab-walkins': {
-        templateUrl: 'templates/tab-walkins.html',
-        controller: 'WalkInsCtrl'
-      }
-    }
-  })
-
-  .state('tab.appts', {
-      url: '/appts',
+    .state('app.appointments', {
+      url: "/appointments",
       views: {
-        'tab-appts': {
-          templateUrl: 'templates/tab-appts.html',
-          controller: 'ApptsCtrl'
+        'menuContent': {
+          templateUrl: "templates/appointments.html",
+          controller: 'AppointmentsCtrl'
         }
       }
     })
-    .state('tab.appt-detail', {
-      url: '/appts/:apptId',
+    
+    
+    .state('app.walkins', {
+      url: "/walkins",
       views: {
-        'tab-appts': {
-          templateUrl: 'templates/appt-detail.html',
-          controller: 'ApptDetailCtrl'
+        'menuContent': {
+          templateUrl: "templates/walkins.html",
+          controller: 'WalkinsCtrl'
         }
       }
     })
-
-  .state('tab.mssgs', {
-    url: '/mssgs',
-    views: {
-      'tab-mssgs': {
-        templateUrl: 'templates/tab-mssgs.html',
-        controller: 'MssgsCtrl'
+    
+    .state('app.messages', {
+      url: "/messages",
+      views: {
+        'menuContent': {
+          templateUrl: "templates/messages.html",
+          controller: 'MessagesCtrl'
+        }
       }
-    }
-  });
-
+    })
+    
+	.state('app.walkin', {
+		url: "/walkins/:playlistId",
+		views: {
+		  'menuContent': {
+			templateUrl: "templates/walkin.html",
+			controller: 'WalkinsCtrl'
+		  }
+		}
+	})
+	
+	.state('app.message', {
+		url: "/messages/:playlistId",
+		views: {
+		  'menuContent': {
+			templateUrl: "templates/message.html",
+			controller: 'MessagesCtrl'
+		  }
+		}
+	})
+	
+	.state('app.appointment', {
+		url: "/appointments/:playlistId",
+		views: {
+		  'menuContent': {
+			templateUrl: "templates/appointment.html",
+			controller: 'AppointmentsCtrl'
+		  }
+		}
+	});
+	
+  
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/walkins');
-
+  $urlRouterProvider.otherwise('/app/appointments');
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,45 +1,209 @@
-angular.module('vpt.controllers', [])
+angular.module('starter.controllers', [])
 
-.controller('WalkInsCtrl', function($scope, WalkIns) {
+.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+  
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
   // listen for the $ionicView.enter event:
-  //
   //$scope.$on('$ionicView.enter', function(e) {
   //});
   
-  $scope.walkIns = WalkIns.all();
-  $scope.remove = function(appt) {
-    WalkIns.remove(appt);
-  }
-})
+  // Form data for the login modal
+  $scope.loginData = {};
 
-.controller('WalkInDetailCtrl', function($scope, $stateParams, WalkIns) {
-  $scope.appt = Appts.get($stateParams.walkInId);
-})
+  // Create the login modal that we will use later
+  $ionicModal.fromTemplateUrl('templates/login.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
 
-.controller('ApptsCtrl', function($scope, Appts) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-  
-  $scope.appts = Appts.all();
-  $scope.remove = function(appt) {
-    Appts.remove(appt);
-  }
-})
-
-.controller('ApptDetailCtrl', function($scope, $stateParams, Appts) {
-  $scope.appt = Appts.get($stateParams.apptId);
-})
-
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
+  // Triggered in the login modal to close it
+  $scope.closeLogin = function() {
+    $scope.modal.hide();
   };
+
+  // Open the login modal
+  $scope.login = function() {
+    $scope.modal.show();
+  };
+
+  // Perform the login action when the user submits the login form
+  $scope.doLogin = function() {
+    console.log('Doing login', $scope.loginData);
+
+    // Simulate a login delay. Remove this and replace with your login
+    // code if using a login system
+    $timeout(function() {
+      $scope.closeLogin();
+    }, 1000);
+  };
+})
+
+.controller('MyCtrl', function($scope) {
+
+
+  var menu = ["Appointments","Walk-Ins","Messages","Check-In"];
+  
+  //,"","","" 
+  
+var SubMenu = new Array(4)
+
+SubMenu [0] = new Array(4)
+SubMenu [0][0] = "Confirm"
+SubMenu [0][1] = "Reschedule"
+SubMenu [0][2] = "Add to Calendar"
+SubMenu [0][3] = "Map It"
+
+SubMenu [1] = new Array(2)
+SubMenu [1][0] = "Beryl"
+SubMenu [1][1] = "engineer"
+
+SubMenu [2] = new Array(3)
+SubMenu [2][0] = "Ann"
+SubMenu [2][1] = "surgeon"
+SubMenu [2][2] = "sf"
+
+SubMenu [3] = new Array(2)
+SubMenu [3][0] = "Bill"
+SubMenu [3][1] = "taxman"
+
+  $scope.groups = [];
+for (var i=0; i<menu.length; i++) {
+    $scope.groups[i] = {
+      name: menu[i],
+      items: []
+    };
+    for (var j=0; j<SubMenu[i].length; j++) {
+      $scope.groups[i].items.push(SubMenu[i][j]);
+    }
+    }
+  
+  /*
+   * if given group is the selected group, deselect it
+   * else, select the given group
+   */
+  $scope.toggleGroup = function(group) {
+    if ($scope.isGroupShown(group)) {
+      $scope.shownGroup = null;
+    } else {
+      $scope.shownGroup = group;
+    }
+  };
+  $scope.isGroupShown = function(group) {
+    return $scope.shownGroup === group;
+  };
+  
+})
+
+.controller('PlaylistsCtrl', function($scope) {
+  $scope.playlists = [
+  
+  	{ title: 'Appointments', id: 1 , icon : 'ion-email',count : '',url : 'appointments'},
+    { title: 'Walk-Ins', id: 2  , icon : 'ion-email',count : '',url : 'walkins'},
+    { title: 'Messages', id: 3  , icon : 'ion-email',count : '2',url : 'messages'},
+    { title: 'Check-In', id: 4  , icon : 'ion-email',count : '',url : ''},
+    { title: 'Settings', id: 5  , icon : 'ion-email',count : '',url : ''},
+    { title: 'Contact Us', id: 6  , icon : 'ion-email' ,count : '',url : ''},
+    { title: 'Our Team', id: 7  , icon : 'ion-email' ,count : '',url : ''}
+  ];
+  
+  $scope.getIcon = function(playlists) {
+    return playlists.icon;
+  };
+  
+})
+
+
+
+.controller('AppointmentsCtrl', function($scope) {
+  $scope.appointments = [
+  
+  	{ title: 'Appointment1', id: 1, status : 'confirmed', therapist : 'Raja', date : '05/07/2015', time : '18:30', added : '01/07/2015 15:36'},
+    { title: 'Appointment2', id: 1, status : 'not-confirmed', therapist : 'Raja', date : '05/07/2015', time : '18:30', added : '01/07/2015 15:36'},
+    { title: 'Appointment3', id: 1, status : 'confirmed', therapist : 'Raja', date : '05/07/2015', time : '18:30', added : '01/07/2015 15:36'},
+    { title: 'Appointment4', id: 1, status : 'not-confirmed', therapist : 'Raja', date : '05/07/2015', time : '18:30', added : '01/07/2015 15:36'},
+    { title: 'Appointment5', id: 1, status : 'not-confirmed', therapist : 'Raja', date : '05/07/2015', time : '18:30', added : '01/07/2015 15:36'},
+    { title: 'Appointment6', id: 1, status : 'confirmed', therapist : 'Raja', date : '05/07/2015', time : '18:30', added : '01/07/2015 15:36'},
+    { title: 'Appointment7', id: 1, status : 'confirmed', therapist : 'Raja', date : '05/07/2015', time : '18:30', added : '01/07/2015 15:36'},
+  ];
+  
+ $scope.isConfirmed = function(appointments) {
+    return appointments.status === 'not-confirmed';
+  };
+  
+  $scope.toggle = function(appointments) {
+   
+   if(appointments.status === 'confirmed')
+    appointments.status = 'not-confirmed';
+    else
+    appointments.status = 'confirmed';
+  };
+  
+   $scope.close = function() {
+  		$ionicHistory.goBack();
+  };
+  
+   $scope.viewDetail = function(appointments) {
+      $state.go("#/app/appointments/:"+appointments.id); 
+    };
+ 
+})
+
+
+.controller('WalkinsCtrl', function($scope) {
+  $scope.walkins = [
+  
+  	{ title: 'Walkin 1', id: 1, status : 'confirmed', therapist : 'Raja', date : '05/07/2015', time : '18:30', added : '01/07/2015 15:36'},
+    { title: 'Walkin 2', id: 1, status : 'not-confirmed', therapist : 'Raja', date : '05/07/2015', time : '18:30', added : '01/07/2015 15:36'},
+    { title: 'Walkin 3', id: 1, status : 'confirmed', therapist : 'Raja', date : '05/07/2015', time : '18:30', added : '01/07/2015 15:36'},
+    { title: 'Walkin 4', id: 1, status : 'not-confirmed', therapist : 'Raja', date : '05/07/2015', time : '18:30', added : '01/07/2015 15:36'},
+    { title: 'Walkin 5', id: 1, status : 'not-confirmed', therapist : 'Raja', date : '05/07/2015', time : '18:30', added : '01/07/2015 15:36'},
+    { title: 'Walkin 6', id: 1, status : 'confirmed', therapist : 'Raja', date : '05/07/2015', time : '18:30', added : '01/07/2015 15:36'},
+    { title: 'Walkin 7', id: 1, status : 'confirmed', therapist : 'Raja', date : '05/07/2015', time : '18:30', added : '01/07/2015 15:36'},
+  ];
+  
+ $scope.isConfirmed = function(walkins) {
+    return walkins.status === 'confirmed';
+  };
+  
+   $scope.toggle = function(walkins) {
+   
+   if(walkins.status === 'confirmed')
+    walkins.status = 'not-confirmed';
+    else
+    walkins.status = 'confirmed';
+  };
+})
+
+
+
+.controller('MessagesCtrl', function($scope) {
+  $scope.messages = [
+  
+  	{ title: 'Message 1', body: 'Message 1', id: 1, status : 'read', date : '05/07/2015', time : '18:30', hasAttach : true},
+    { title: 'Message 2', body: 'Message 1',  id: 1, status : 'not-read', date : '05/07/2015', time : '18:30', hasAttach : false},
+    { title: 'Message 3', body: 'Message 1', id: 1, status : 'read', date : '05/07/2015', time : '18:30', hasAttach : false},
+    { title: 'Message 4', body: 'Message 1', id: 1, status : 'not-read', date : '05/07/2015', time : '18:30', hasAttach : true},
+    { title: 'Message 5', body: 'Message 1', id: 1, status : 'not-read', date : '05/07/2015', time : '18:30', hasAttach : true},
+    { title: 'Message 6', body: 'Message 1', id: 1, status : 'read', date : '05/07/2015', time : '18:30', hasAttach : false},
+    { title: 'Message 7', body: 'Message 1', id: 1, status : 'read', date : '05/07/2015', time : '18:30', hasAttach : true},
+  ];
+  
+ $scope.isRead = function(Message) {
+    return Message.status === 'not-read';
+  };
+  
+   $scope.hasAttach = function(Message) {
+    return Message.hasAttach;
+  };
+  
+   $scope.read = function(Message) {
+       Message.status = 'read';
+  };
+})
+
+
+.controller('PlaylistCtrl', function($scope, $stateParams) {
 });
